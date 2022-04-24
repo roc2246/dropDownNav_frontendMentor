51cf23d26bb4a183699c35783345c7a443e97d90
Riley <riley.childs@yahoo.com>
2022-04-24 13:39:44 -0400

    Created js for dropdown menu

---
+++ public/js/dropdown.js
@@ -0,0 +1,15 @@
+const mainNav = document.getElementById("main-nav");
+const opt = document.getElementsByClassName("subnav");
+
+Object.keys(opt).forEach((option) => {
+    opt[option].style.display = "none";
+  });
+
+const displaySubOpts = (o) => {
+  opt[o].style.display = "block";
+}
+
+const hideSubOpts = (o) => {
+    opt[o].style.display = "none";
+   }
+   
