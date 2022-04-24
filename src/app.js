4e58531679f4b2a24bd435f278287757a22adb2c
Riley <riley.childs@yahoo.com>
2022-04-24 15:43:20 -0400

    Organized layout successfully

--- src/app.js
+++ src/app.js
@@ -3,5 +3,11 @@ const app = express()
 
 app.use(express.json())
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Drop-Down Challenge',
+        name: 'Riley Childs'
+    })
+})
 
 module.exports = app
