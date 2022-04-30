700155ec60f8f387e773e84be1142bdbd49a0b24
Riley <riley.childs@yahoo.com>
2022-04-30 08:34:52 -0400

    Collapsable mobile nav options are now working

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -7,28 +7,32 @@ Object.keys(opt).forEach((option) => {
 
 const displaySubOpts = (o) => {
   opt[o].style.display = "block";
-  arrowIcon[o].setAttribute('d', 'm1 5 4-4 4 4');
+  arrowIcon[o].setAttribute("d", "m1 5 4-4 4 4");
+
+  opt[o].parentElement.setAttribute("ontouchstart", "hideSubOpts(" + o + ")");
 };
 
 const hideSubOpts = (o) => {
   opt[o].style.display = "none";
-  arrowIcon[o].setAttribute('d', 'm1 1 4 4 4-4');
+  arrowIcon[o].setAttribute("d", "m1 1 4 4 4-4");
+  opt[o].parentElement.setAttribute("ontouchstart", "displaySubOpts(" + o + ")");
 };
 
-
 //For Mobile Site
-const navigation = document.getElementById("mobile-nav").querySelector("ul")
-const hamburgerIcon = document.getElementById("mobile-nav").querySelector("svg")
-const closeMobile = document.getElementById("mobileClose")
+const navigation = document.getElementById("mobile-nav").querySelector("ul");
+const hamburgerIcon = document
+  .getElementById("mobile-nav")
+  .querySelector("svg");
+const closeMobile = document.getElementById("mobileClose");
 
 const displayHamburger = () => {
   navigation.style.display = "flex";
   hamburgerIcon.style.display = "none";
-  closeMobile.style.display="block";
-}
+  closeMobile.style.display = "block";
+};
 
 const hideHamburger = () => {
   navigation.style.display = "none";
   hamburgerIcon.style.display = "inline";
-  closeMobile.style.display="none";
-}
+  closeMobile.style.display = "none";
+};
